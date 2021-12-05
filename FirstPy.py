001ce457aaf3f7f5fa06238a1e40c0e4643b7c08
arnavdhumal <95065335+arnavdhumal@users.noreply.github.com>
2021-12-04 17:26:55 -0800

    Add files via upload

---
+++ FirstPy.py
@@ -0,0 +1,20 @@
+
+a = input("First Number? ")
+b = input("Second Number? ")
+c = float(a)
+d = float(b)
+def Addition(num1, num2):
+    return num1 + num2
+def Subtraction(num1, num2):
+    return num1 - num2
+def Multiplication(num1, num2):
+    return num1 * num2
+def Division(num1, num2):
+    return num1 / num2
+def Percentage(num1, num2):
+    return num1 % num2
+print(Addition(c, d))
+print(Subtraction(c, d))
+print(Multiplication(c, d))
+print(Division(c, d))
+print(Percentage(c, d))
