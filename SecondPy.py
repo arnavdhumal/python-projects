fa371436537683dc04c2ce67b32983e3353ff993
arnavdhumal <arnavdhumal>
2022-01-14 19:27:59 -0800

    vh

--- SecondPy.py
+++ SecondPy.py
@@ -1,16 +1,27 @@
 Yes = "Good Job! Your answer is correct!"
 No = "Oops, You're wrong. Try again."
 OhNo = "Looks like it didn't work. Please answer with Yes or No. Reload to try again."
-FQ = input("Do you know Python? Type Yes or No. ")
-if FQ == "Yes":
-    print("So you know, I'll give you some questions")
-    while True:
-        SQ = input("What is the word one would use to define a function? ")
-        if SQ == "def":
-            print(Yes)
-            break
-        print(No)
-elif FQ == "No":
-    print("That's ok. Go to https://www.w3schools.com/python/default.asp to learn the language.")
-else:
-    print(OhNo)
+
+while True:
+    FQ = input("Do you know Python? Type Yes or No. ")
+    if FQ == "Yes":
+        print("So you know, I'll give you some questions")
+        while True:
+            SQ = input("What is the word one would use to define a function? ")
+            if SQ == "def":
+                print(Yes)
+                break
+            print(No)
+        while True:
+            SQ = input("Well, second question... What is the word one would use to define a function? ")
+            if SQ == "def2":
+                print(Yes)
+                break
+            print(No)
+        break
+    elif FQ == "No":
+        print("That's ok. Go to https://www.w3schools.com/python/default.asp to learn the language.")
+        break
+    else:
+        print(OhNo)
+        continue
